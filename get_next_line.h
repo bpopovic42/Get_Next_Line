88e8b497d4633dee63d61a6637add96cb00bccf9
Boris POPOVIC <bopopovi@student.42.fr>
2018-05-23 22:53:01 +0200

    Modified buffer size

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 20:45:59 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/05/22 16:33:51 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/05/23 19:03:58 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GNL_H
 # define GNL_H
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 3
 
 # include "./libft/libft.h"
 
