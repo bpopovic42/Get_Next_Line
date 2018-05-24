f1d8b10a84fd6dd021d329a699840d00d5ca116a
Boris POPOVIC <bopopovi@student.42.fr>
2018-05-24 21:39:29 +0200

    Protected function

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 19:26:49 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/04/25 16:39:35 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/05/24 21:38:35 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ char	*ft_strdup(const char *s1)
 {
 	char *res;
 
-	if (!(res = (char*)malloc(sizeof(*res) * ft_strlen(s1) + 1)))
-		return (NULL);
-	res = ft_strcpy(res, s1);
+	res = NULL;
+	if (s1)
+	{
+		if (!(res = (char*)malloc(sizeof(*res) * ft_strlen(s1) + 1)))
+			return (NULL);
+		res = ft_strcpy(res, s1);
+	}
 	return (res);
 }
