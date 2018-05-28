af9b76e09ee4693a0fa3c6a336e95e49f4e89dd1
Boris POPOVIC <bopopovi@student.42.fr>
2018-05-28 17:07:07 +0200

    Normed file

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,46 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 20:42:42 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/05/28 00:53:34 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/05/28 16:04:54 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <unistd.h> //
+#include <unistd.h>
 
-t_fd	*new_fd(int fd, char *reminder);
-char	*get_fd(t_hash **list, int fd);
-void	save_remain(t_hash **list, char *remainder, int fd);
-int		is_line(char **str);
-
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	static t_hash	*list;
-	char			*reminder;
+	char			*remain;
 	char			buff[BUFF_SIZE + 1];
 	int				i;
 	int				ret;
 
 	ret = 1;
-	reminder = get_fd(&list, fd);
-	while ((i = ft_strchrin(reminder, '\n')) < 0 && ret)
+	if (fd < 0 || !(line))
+		return (-1);
+	remain = get_fd(&list, fd);
+	while ((i = ft_strchrin(remain, '\n')) < 0 && ret)
 	{
 		if ((ret = (int)read(fd, buff, BUFF_SIZE)) < 0)
 			return (-1);
 		buff[ret] = '\0';
-		reminder = ft_strappend(reminder, buff);
+		remain = ft_strappend(remain, buff);
 	}
-	*line = (i >= 0) ? ft_strsub(reminder, 0, (size_t)i) : ft_strdup(reminder);
+	*line = (i >= 0) ? ft_strsub(remain, 0, (size_t)i) : ft_strdup(remain);
 	if (i >= 0)
-		save_remain(&list, ft_strdup(reminder + i + 1), fd);
-	ret = (reminder[0]) ? 1 : ret;
-	ft_strdel(&reminder);
+		save_remain(&list, ft_strdup(remain + i + 1), fd);
+	ret = (remain[0]) ? 1 : ret;
+	ft_strdel(&remain);
 	return (ret);
 }
 
-void		save_remain(t_hash **list, char *remainder, int fd)
+void		save_remain(t_hash **list, char *remain, int fd)
 {
 	t_hash	*ptr;
 
-	ptr = ft_hashnew(fd, remainder, (sizeof(*remainder) * ft_strlen(remainder)) + 1); // SOLVED
+	ptr = ft_hashnew(fd, remain, (sizeof(*remain) * ft_strlen(remain)) + 1);
+	ft_strdel(&remain);
 	ft_hashpush(list, ptr);
 }
 
