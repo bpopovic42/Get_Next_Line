63605d8e49a9e0645b4828c8c966837745b04cef
Boris POPOVIC <bopopovi@student.42.fr>
2018-06-01 19:07:30 +0200

    [ADD] New hash function

--- libft/srcs/ft_hashget.c
+++ libft/srcs/ft_hashget.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 23:52:12 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/06/01 17:31:21 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/06/01 17:47:32 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@ void	*ft_hashget(t_hash **list, int key)
 	if (*list)
 	{
 		ptr = ft_hashpop(list, key);
-		if (!(data = ft_memdup(ptr->data, ptr->data_size)))
-			return (NULL);
-		ft_hashdel(&ptr);
-		return (data);
+		if (ptr)
+		{
+			if (!(data = ft_memdup(ptr->data, ptr->data_size)))
+				return (NULL);
+			ft_hashdel(&ptr);
+			return (data);
+		}
 	}
 	return (NULL);
 }
